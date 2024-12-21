511982c23f0f2bed64868bfe8c57984f7106f6d3
ayansheikh6600 <ayansheikh6600@gmail.com>
2024-12-21 20:32:48 +0500

    Python Hellow Worls

---
+++ hellow.py
@@ -0,0 +1,58 @@
+# print("hellow world!")
+# output: hellow world!
+
+
+# name = "Ayan"
+# age = 19
+# is_developer = True
+
+
+# print(name,age,is_developer)
+# output:Ayan 19 True
+
+
+# name = input("Enter your Name")
+# age = input("Enter your Age")
+
+# print(age, name)
+
+# name = input("Enter your Name")
+# age = input("Enter your Age")
+
+# print(f"My name is {name} and my age is {age}")
+# My name is Ayan and my age is 19
+
+# num1 = int(input("Enter your num 1"))
+# num2 = int(input("Enter your num 2"))
+
+# sum = num1 + num2
+
+# print(sum)
+# Enter your num 119
+# Enter your num 220
+# 39
+
+# if False :
+#     print("Ayan")
+# else:
+#         print("False")
+
+# fruits = ["apple", "banana", "cherry"]
+
+# for f in fruits:
+#     print(f)
+
+# count = 1
+# while count <= 5:
+#     print(count)
+#     count += 1
+
+# for num in range(10):
+#     if num == 5:
+#         break
+#     print(num)
+
+# for num in range(10):
+#     if num == 5:
+#         continue
+#     print(num)
